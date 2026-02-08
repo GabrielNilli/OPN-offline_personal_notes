5b9ff93984a62486d910746c50ecfede5d52875b
Gabriel <nilligabriel@gmail.com>
2026-02-08 13:02:00 +0100

    Test change color;

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 1, 247, 75)),
+        colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 247, 1, 1)),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
